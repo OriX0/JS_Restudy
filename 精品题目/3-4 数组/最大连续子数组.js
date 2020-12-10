a9a0d0e407e6bd26f399afb7bd03dcd8c16a12ca
OriX <1098888910@qq.com>
2020-12-10 21:50:44 +0800

    🤔 重新过了一遍数组知识点 并增加了一个新的精品提

---
+++ 精品题目/3-4 数组/最大连续子数组.js
@@ -0,0 +1,17 @@
+/*
+ * @Description:
+ * @Version: 1.0
+ * @Author: OriX
+ * @Date: 2020-12-10 14:45:01
+ * @LastEditors: OriX
+ * @LastEditTime: 2020-12-10 14:45:46
+ * @Copyright (C) 2020 OriX. All rights reserved.
+ */
+/*
+
+输入是以数字组成的数组，例如 arr = [1, -2, 3, 4, -9, 6].
+任务是：找出所有项的和最大的 arr 数组的连续子数组。
+写出函数 getMaxSubSum(arr)，用其找出并返回最大和。
+如果所有项都是负数，那就一个项也不取（子数组是空的），所以返回的是
+
+*/
