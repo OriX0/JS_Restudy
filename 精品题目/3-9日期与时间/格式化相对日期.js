a41a3d18e9dc2b1f804247bb0ae9b4cb672991cb
OriX <1098888910@qq.com>
2020-12-16 15:05:00 +0800

    📦 Add:一道综合运用日期相关函数的题目

---
+++ 精品题目/3-9日期与时间/格式化相对日期.js
@@ -0,0 +1,16 @@
+/*
+ * @Description:
+ * @Version: 1.0
+ * @Author: OriX
+ * @Date: 2020-12-16 14:58:07
+ * @LastEditors: OriX
+ * @LastEditTime: 2020-12-16 14:58:08
+ * @Copyright (C) 2020 OriX. All rights reserved.
+ */
+// 格式化相对日期
+/* 写一个函数 formatDate(date)，能够对 date 进行如下格式化：
+如果 date 距离现在不到 1 秒，输出 "right now"。
+否则，如果 date 距离现在不到 1 分钟，输出 "n sec. ago"。
+否则，如果不到 1 小时，输出 "m min. ago"。
+否则，以 "DD.MM.YY HH:mm" 格式输出完整日期。即："day.month.year hours:minutes"，
+全部以两位数格式表示，例如：31.12.16 10:00。*/
